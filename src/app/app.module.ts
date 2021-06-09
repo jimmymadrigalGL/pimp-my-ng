13c7af885e2f5e770e17bba8f69978d0d2031194
jimmymadrigalGL <jimmy.madrigal@gorillalogic.com>
2021-06-08 22:07:33 -0600

    SVG font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { AppComponent } from './app.component';
 import { TreeComponent } from './tree/tree.component';
 import { NodeComponent } from './node/node.component';
 import { FacadeService } from './services/facade.service';
 import { Observable, of } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule, AngularSvgIconModule.forRoot()],
   declarations: [AppComponent, TreeComponent, NodeComponent],
   bootstrap: [AppComponent],
   providers: [
